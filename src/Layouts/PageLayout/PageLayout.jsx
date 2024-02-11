50a3d57053954b6004b90bf8775e99641e39e10b
Nico Marsh <nicolasmarsh94@gmail.com>
2024-02-11 20:56:52 -0300

    Add Spinner and Navbar to PageLayout component

--- src/Layouts/PageLayout/PageLayout.jsx
+++ src/Layouts/PageLayout/PageLayout.jsx
@@ -1,6 +1,9 @@
-import { Box, Flex } from "@chakra-ui/react"
+import { Box, Flex, Spinner } from "@chakra-ui/react"
 import Sidebar from "../../components/Sidebar/Sidebar"
 import { useLocation } from "react-router-dom"
+import { useAuthState } from "react-firebase-hooks/auth"
+import { auth } from "../../firebase/firebase"
+import Navbar from "../../components/Navbar/Navbar"
 
 /* instead of adding the Sidebar to every page, we can add it to only the PageLayout
    component and wrap the children with it. This way, we can have a sidebar on 
@@ -8,16 +11,27 @@ import { useLocation } from "react-router-dom"
 
 const PageLayout = ({children}) => {
   const { pathname } = useLocation();
+  const [user, loading, error] = useAuthState(auth);
+  const canRenderSideBar = pathname !== "/auth" && user;
+  const canRenderNavbar = !user && !loading && pathname !== "/auth";
+  
+  const checkingUserIsAuth = !user && loading;
+  if (checkingUserIsAuth) return <PageLayoutSpinner />;
+
+  if (!user )
+
   return (
-    <Flex>
+    <Flex flexDir={canRenderNavbar ? "column" : "row"}>
         {/* sidebar on the left */}
         { pathname !== "/auth" ? (
             <Box w={{base:"70px", md:"240px"}}>
                 <Sidebar />
             </Box>
             ) : null }
+        {/* Navbar */}
+        {canRenderNavbar ? <Navbar /> : null}
         {/* the page content on the right */}
-        <Box flex={1} width={{ base: "calc(100% - 70px)", md: "calc(100% - 240px)"}}>
+        <Box flex={1} width={{ base: "calc(100% - 70px)", md: "calc(100% - 240px)"}} mx={"auto"}>
             {children}
         </Box>
     </Flex>
@@ -25,3 +39,11 @@ const PageLayout = ({children}) => {
 }
 
 export default PageLayout
+
+const PageLayoutSpinner = () => {
+  return (
+    <Flex flexDir="column" h="100vh" alignItems={"center"} justifyContent={"center"}>
+      <Spinner size="xl" />
+    </Flex>
+  );
+}
