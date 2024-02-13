3e5f68f41afb224c52759ae8448905e814c4bf83
Nico Marsh <nicolasmarsh94@gmail.com>
2024-02-12 21:00:31 -0300

    Refactor Signup component

--- src/components/AuthForm/Signup.jsx
+++ src/components/AuthForm/Signup.jsx
@@ -1,90 +1,79 @@
 import { ViewIcon, ViewOffIcon } from "@chakra-ui/icons";
-import { InputGroup, Input, InputRightElement, Button } from "@chakra-ui/react";
+import { Alert, AlertIcon, Button, Input, InputGroup, InputRightElement } from "@chakra-ui/react";
 import { useState } from "react";
 import useSignUpWithEmailAndPassword from "../../hooks/useSignUpWithEmailAndPassword";
-import {
-  Alert,
-  AlertIcon,
-  AlertDescription,
-} from '@chakra-ui/react'
-
-
 
 const Signup = () => {
-  const [inputs, setInputs] = useState({
-    fullName: "",
-    username: "",
-    email: "",
-    password: "",
-  });
-  const [showPassword, setShowPassword] = useState(false);
-  const { loading, error, signup } = useSignUpWithEmailAndPassword();
+	const [inputs, setInputs] = useState({
+		fullName: "",
+		username: "",
+		email: "",
+		password: "",
+	});
+	const [showPassword, setShowPassword] = useState(false);
+	const { loading, error, signup } = useSignUpWithEmailAndPassword();
 
-  return (
-    <>
-      <Input
-        placeholder="Email"
-        type="email"
-        fontSize={14}
-        value={inputs.email}
-        size={"sm"}
-        onChange={(e) => setInputs({ ...inputs, email: e.target.value })}
-      />
-      <Input
-        placeholder="Username"
-        type="text"
-        fontSize={14}
-        value={inputs.username}
-        size={"sm"}
-        onChange={(e) => setInputs({ ...inputs, username: e.target.value })}
-      />
-      <Input
-        placeholder="Full Name"
-        type="text"
-        fontSize={14}
-        value={inputs.fullName}
-        size={"sm"}
-        onChange={(e) => setInputs({ ...inputs, fullName: e.target.value })}
-      />
-      <InputGroup>
-        <Input
-          placeholder="Password"
-          type={showPassword ? "text" : "password"}
-          fontSize={14}
-          value={inputs.password}
-          size={"sm"}
-          onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
-        />
-        <InputRightElement h={"full"}>
-          <Button
-            variant={"ghost"}
-            size={"sm"}
-            onClick={() => setPassword(!showPassword)}
-          >
-            {showPassword ? <ViewIcon /> : <ViewOffIcon />}
-          </Button>
-        </InputRightElement>
-      </InputGroup>
+	return (
+		<>
+			<Input
+				placeholder='Email'
+				fontSize={14}
+				type='email'
+				size={"sm"}
+				value={inputs.email}
+				onChange={(e) => setInputs({ ...inputs, email: e.target.value })}
+			/>
+			<Input
+				placeholder='Username'
+				fontSize={14}
+				type='text'
+				size={"sm"}
+				value={inputs.username}
+				onChange={(e) => setInputs({ ...inputs, username: e.target.value })}
+			/>
+			<Input
+				placeholder='Full Name'
+				fontSize={14}
+				type='text'
+				size={"sm"}
+				value={inputs.fullName}
+				onChange={(e) => setInputs({ ...inputs, fullName: e.target.value })}
+			/>
+			<InputGroup>
+				<Input
+					placeholder='Password'
+					fontSize={14}
+					type={showPassword ? "text" : "password"}
+					value={inputs.password}
+					size={"sm"}
+					onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
+				/>
+				<InputRightElement h='full'>
+					<Button variant={"ghost"} size={"sm"} onClick={() => setShowPassword(!showPassword)}>
+						{showPassword ? <ViewIcon /> : <ViewOffIcon />}
+					</Button>
+				</InputRightElement>
+			</InputGroup>
 
-      {error && (
-        <Alert status="error" fontSize={13} p={2} borderRadius={4}>
-          <AlertIcon fontSize={12}/>
-            {error.message}
-        </Alert>
-      )}
+			{error && (
+				<Alert status='error' fontSize={13} p={2} borderRadius={4}>
+					<AlertIcon fontSize={12} />
+					{error.message}
+				</Alert>
+			)}
 
-      <Button
-        w={"full"}
-        colorScheme="blue"
-        size="sm"
-        fontSize={14}
-        isLoading={loading}
-        onClick={() => signup(inputs)}
-      >
-        Sign up
-      </Button>
-    </>
-  );
+			<Button
+				w={"full"}
+				colorScheme='blue'
+				size={"sm"}
+				fontSize={14}
+				isLoading={loading}
+				onClick={() => signup(inputs)}
+			>
+				Sign Up
+			</Button>
+		</>
+	);
 };
 
 export default Signup;
