922d663ea359f76c8b104eaa850d72c0040ae39f
Nico Marsh <nicolasmarsh94@gmail.com>
2024-02-13 18:49:57 -0300

    Refactor SuggestedUsers component and add useGetSuggestedUsers hook

--- src/components/SuggestedUsers/SuggestedUsers.jsx
+++ src/components/SuggestedUsers/SuggestedUsers.jsx
@@ -1,35 +1,41 @@
-import { Box, Flex, Link, Text, VStack } from "@chakra-ui/react"
-import SuggestedHeader from "./SuggestedHeader"
-import SuggestedUser from "./SuggestedUser"
+import { Box, Flex, Link, Text, VStack } from "@chakra-ui/react";
+import SuggestedHeader from "./SuggestedHeader";
+import SuggestedUser from "./SuggestedUser";
+import useGetSuggestedUsers from "../../hooks/useGetSuggestedUsers";
 
 const SuggestedUsers = () => {
-  return (
-    <VStack py={8} px={6} gap={4}>
-        <SuggestedHeader />
+	const { isLoading, suggestedUsers } = useGetSuggestedUsers();
 
-        <Flex alignItems={"center"} justifyContent={"space-between"} w={"full"}>
-            <Text fontSize={12} fontWeight={"bold"} color={"gray.500"}>
-                Suggestions for you
-            </Text>
-            <Text fontSize={12} fontWeight={"bold"} _hover={{ color:"gray.400"}} cursor={"pointer"}>
-                See All
-            </Text>
-        </Flex>
+	// optional: render loading skeleton
+	if (isLoading) return null;
 
-        <SuggestedUser name="suggesteduser1" followers={1000} avatar="https://bit.ly/dan-abramov" />
-        <SuggestedUser name="suggesteduser2" followers={2000} avatar="https://bit.ly/ryan-florence" />
-        <SuggestedUser name="suggesteduser3" followers={3000} avatar="https://bit.ly/code-beast" />
+	return (
+		<VStack py={8} px={6} gap={4}>
+			<SuggestedHeader />
 
-        <Box 
-            fontSize={12} color={"gray.500"} mt={5} alignSelf={"start"}>
-                © 2024 Built By{" "}
-                <Link href="https://www.linkedin.com/in/nicolasmarsh/" target="_blank"
-                    color={"blue.500"} fontSize={14}>
-                        Nico Marsh
-                </Link>
-        </Box>
-    </VStack>
-  )
-}
+			{suggestedUsers.length !== 0 && (
+				<Flex alignItems={"center"} justifyContent={"space-between"} w={"full"}>
+					<Text fontSize={12} fontWeight={"bold"} color={"gray.500"}>
+						Suggested for you
+					</Text>
+					<Text fontSize={12} fontWeight={"bold"} _hover={{ color: "gray.400" }} cursor={"pointer"}>
+						See All
+					</Text>
+				</Flex>
+			)}
 
-export default SuggestedUsers
+			{suggestedUsers.map((user) => (
+				<SuggestedUser user={user} key={user.id} />
+			))}
+
+			<Box fontSize={12} color={"gray.500"} mt={5} alignSelf={"start"}>
+				© 2024 Built By{" "}
+				<Link href='https://www.linkedin.com/in/nicolasmarsh/' target='_blank' color='blue.500' fontSize={14}>
+					Nico Marsh
+				</Link>
+			</Box>
+		</VStack>
+	);
+};
+
+export default SuggestedUsers;
